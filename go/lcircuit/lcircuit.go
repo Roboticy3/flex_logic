a7c9a320888bf17d7b79a162338991a18b0357c9
Pablo Ibarz <fribarz123@gmail.com>
2025-11-21 13:18:08 -0500

    began testing net controller

--- go/lcircuit/lcircuit.go
+++ go/lcircuit/lcircuit.go
@@ -1,10 +1,17 @@
 package lcircuit
 
-/*
-	a pin on a specific component in a circuit
+import (
+	"fmt"
+	"sort"
+)
 
-	`valid` will be false by default, making new pins (eg `var zero LPin[S,T]`)
-	is an easy way to add empty entries.
+/*
+a pin on a specific component in a circuit
+
+`valid` will be false by default, making new pins (eg `var zero LPin[S,T]`)
+is an easy way to add empty entries.
+
+`nets` will remain sorted for easy search.
 */
 type LPin[S LState, T LTime] struct {
 	nets  []Label
@@ -15,6 +22,24 @@ func (pin LPin[S, T]) IsEmpty() bool {
 	return !pin.valid
 }
 
+func (pin LPin[S, T]) Len() int {
+	return len(pin.nets)
+}
+
+func (pin LPin[S, T]) Less(i, j int) bool {
+	return pin.nets[i] < pin.nets[j]
+}
+
+func (pin LPin[S, T]) Swap(i, j int) {
+	pin.nets[i], pin.nets[j] = pin.nets[j], pin.nets[i]
+}
+
+/*
+A net between circuit pins. Drives connected nets with its state and processes
+events with its `tid`.
+
+`pins` will remain sorted for easy merging.
+*/
 type LNet[S LState, T LTime] struct {
 	pins  []Label
 	tid   Label
@@ -25,13 +50,25 @@ func (net LNet[S, T]) IsEmpty() bool {
 	return len(net.pins) == 0
 }
 
-/*
- Base type for a circuit
-	`netlist`	labeling over nets on the circuit
-	`pinlist` labeling over pins on the circuit
+func (net LNet[S, T]) Len() int {
+	return len(net.pins)
+}
 
-	Each pin belongs to n nets
-	Each net sees p pins and has an optional gate type.
+func (net LNet[S, T]) Less(i, j int) bool {
+	return net.pins[i] < net.pins[j]
+}
+
+func (net LNet[S, T]) Swap(i, j int) {
+	net.pins[i], net.pins[j] = net.pins[j], net.pins[i]
+}
+
+/*
+	 Base type for a circuit
+		`netlist`	labeling over nets on the circuit
+		`pinlist` labeling over pins on the circuit
+
+		Each pin belongs to n nets
+		Each net sees p pins and has an optional gate type.
 */
 type LCircuit[S LState, T LTime] struct {
 	netlist   LLabeling[LNet[S, T]]
@@ -67,9 +104,9 @@ type LCGateController[S LState, T LTime] struct {
 }
 
 /*
-	Adding a gate of type `gname` induces pins and nets onto the circuit.
+Adding a gate of type `gname` induces pins and nets onto the circuit.
 
-	O(t + p) for t types and p pins.
+O(t + p) for t types and p pins.
 */
 func (gc LCGateController[S, T]) AddGate(gname string) Label {
 
@@ -91,18 +128,21 @@ func (gc LCGateController[S, T]) AddGate(gname string) Label {
 		zero,
 	}, 0)
 
+	//Drizzle new pins into the pinlist. The new net's pin array will be sorted
+	//as a result :)
 	pid := 0
 	for i := range pincount {
 		pins[i] = gc.pinlist.Add(LPin[S, T]{[]Label{nid}, true}, pid)
+		pid = int(pins[i]) + 1
 	}
 
 	return nid
 }
 
 /*
-	Remove the gate at `gid`. Returns true if the gate existed and false otherwise
+Remove the gate at `gid`. Returns true if the gate existed and false otherwise
 
-	O(pqu) for p pins, q, u average connections on nets, pins.
+O(pqu) for p pins, q, u average connections on nets, pins.
 */
 func (gc LCGateController[S, T]) RemoveGate(gid Label) bool {
 
@@ -126,9 +166,9 @@ func (gc LCGateController[S, T]) RemoveGate(gid Label) bool {
 }
 
 /*
-	List valid gate ids
+List valid gate ids
 
-	O(n) for n gates.
+O(n) for n gates.
 */
 func (gc LCGateController[S, T]) ListGateIds() []Label {
 	result := []Label{}
@@ -146,18 +186,18 @@ type LCPinController[S LState, T LTime] struct {
 }
 
 /*
-	Add a pin to the circuit.
+Add a pin to the circuit.
 
-	If the provided `nid` is `LABEL_EMPTY`, the pin is added with no associated
-	net
+If the provided `nid` is `LABEL_EMPTY`, the pin is added with no associated
+net.
 
-	If `nid` is a valid label, but doesn't correspond to a valid net, the function
-	fails and returns `LABEL_EMPTY`
+If `nid` is a valid label, but doesn't correspond to a valid net, the function
+fails and returns `LABEL_EMPTY`
 
-	If `nid` corresponds to a valid net, the pin is added to the circuit and
-	connected to the net.
+If `nid` corresponds to a valid net, the pin is added to the circuit and
+connected to the net.
 
-	O(q) for q average connections on a net.
+O(q log q) for q average connections on a net.
 */
 func (pc LCPinController[S, T]) AddPin(nid Label) Label {
 
@@ -176,19 +216,22 @@ func (pc LCPinController[S, T]) AddPin(nid Label) Label {
 			return result
 		}
 	}
+
 	p_net.pins = append(p_net.pins, result)
+	sort.Sort(p_net)
+
 	return result
 }
 
 /*
-	Remove a pin at `pid` from the circuit. Returns true if the pin existed and
-	false otherwise.
+Remove a pin at `pid` from the circuit. Returns true if the pin exists.
+False otherwise.
 
-	The pin is disconnected from any associated nets. If these nets become fully
-	disconnected, they are removed.
+The pin is disconnected from any associated nets. If these nets become fully
+disconnected, they are removed.
 
-	O(qu) for q, u average connections on nets, pins. In a standard digital logic
-	circuit, pins should not exceed two nets- one component and one wire.
+O(u log q) for q, u average connections on nets, pins. In a standard digital logic
+circuit, pins should not exceed two nets- one component and one wire.
 */
 func (pc LCPinController[S, T]) RemovePin(pid Label) bool {
 
@@ -198,6 +241,7 @@ func (pc LCPinController[S, T]) RemovePin(pid Label) bool {
 	}
 
 	//find this pin in the connected nets and remove it
+	fmt.Println("Detaching pin ", pid, " from nets ", p_pin.nets)
 	for _, nid := range p_pin.nets {
 		LCNetController[S, T](pc).Detach(nid, pid)
 	}
@@ -208,17 +252,27 @@ func (pc LCPinController[S, T]) RemovePin(pid Label) bool {
 	return true
 }
 
+func (pc LCPinController[S, T]) ListPins() []Label {
+	result := []Label{}
+	for pid, pin := range pc.pinlist {
+		if !pin.IsEmpty() {
+			result = append(result, Label(pid))
+		}
+	}
+	return result
+}
+
 type LCNetController[S LState, T LTime] struct {
 	*LCircuit[S, T]
 }
 
 /*
-	Detach net `nid` from pin `pid`
+Detach net `nid` from pin `pid`
 
-	If detaching this pin would leave net `nid` with zero connections, the net
-	is removed from the circuit.
+If detaching this pin would leave net `nid` with zero connections, the net
+is removed from the circuit.
 
-	O(q) for q average connections on a net.
+O(log q) for q average connections on a net.
 */
 func (nc LCNetController[S, T]) Detach(nid Label, pid Label) bool {
 
@@ -230,14 +284,134 @@ func (nc LCNetController[S, T]) Detach(nid Label, pid Label) bool {
 		return true
 	}
 
-	for i, pid2 := range p_net.pins {
-		if pid == pid2 {
-			//lazy remove (O(1))
-			p_net.pins[i] = p_net.pins[len(p_net.pins)-1]
-			p_net.pins = p_net.pins[:len(p_net.pins)-1]
-			return true
+	//binary search O(log q)
+	i := sort.Search(len(p_net.pins), func(i int) bool { return p_net.pins[i] >= pid })
+	if i < 0 || i >= len(p_net.pins) {
+		return false
+	}
+
+	//lazy remove (O(1))
+	p_net.pins[i] = p_net.pins[len(p_net.pins)-1]
+	p_net.pins = p_net.pins[:len(p_net.pins)-1]
+
+	return true
+}
+
+/*
+Attach net `nid` to pin `pid`. If either do not exist, or the connection
+already exists, return false.
+
+O(log q) for q average connections on a net.
+*/
+func (nc LCNetController[S, T]) Attach(nid Label, pid Label) bool {
+	return false
+}
+
+/*
+Merge nets at `nids` together. The first valid element of `nids` is the target
+net for the merge, so its `state` and `tid` will dominate.
+
+O(nq log q) for n valid elements of `nids`, q average connections on a net.
+*/
+func (nc LCNetController[S, T]) Merge(nids []Label) {
+
+	target := LABEL_EMPTY
+	target_i := -1 //store the index where the target was found for slicing
+	for i, nid := range nids {
+		p_net := nc.netlist.Get(nid)
+		if p_net != nil {
+			target = nid
+			target_i = i
 		}
 	}
 
-	return false
+	if target == LABEL_EMPTY {
+		return
+	}
+
+	if target_i == len(nids)-1 {
+		return
+	}
+
+	nids = nids[target_i+1:]
+
+	//merge each valid net after target into target
+	for _, nid := range nids {
+		nc.MergeTwo(target, nid)
+	}
+}
+
+/*
+Merge net nid2 into nid1. If nid2 is invalid, return false. If nid1 is
+invalid, replace it with nid2.
+
+O(q log q) for q average connections on a net.
+*/
+func (nc LCNetController[S, T]) MergeTwo(nid1 Label, nid2 Label) bool {
+	p_source := nc.netlist.Get(nid2)
+	if p_source == nil {
+		return false
+	}
+
+	p_target := nc.netlist.Get(nid1)
+	if p_target == nil {
+		if nid1 == LABEL_EMPTY {
+			return false
+		}
+
+		//In this case, just make a new target with no connections.
+		new_target := LNet[S, T]{
+			p_source.pins,
+			p_source.tid,
+			p_source.state,
+		}
+		nc.netlist[nid1] = new_target
+	} else {
+		//Otherwise, merge the pin arrays, use target for the tid and state
+		//Since net pins are sorted, the merge is linear with interlacing:
+		pins := make([]Label, len(p_target.pins)+len(p_source.pins))
+		i, j := 0, 0
+		for p := range pins {
+			//push target
+			if i < len(p_target.pins) && p_target.pins[i] < p_source.pins[j] {
+				pins[p] = p_target.pins[i]
+				i++
+				//remove duplicates
+			} else if i < len(p_target.pins) && j < len(p_source.pins) && p_target.pins[i] == p_source.pins[j] {
+				pins[p] = p_target.pins[i]
+				pins = pins[:len(pins)-1]
+				i++
+				j++
+				//push source
+			} else if j < len(p_source.pins) {
+				pins[p] = p_source.pins[j]
+				j++
+			}
+		}
+
+		//set pins, but keep the target's tid and state
+		p_target.pins = pins
+	}
+
+	//retarget all pins pointing at source to point at target O(q log q)
+	for _, pid := range p_target.pins {
+		p_pin := nc.pinlist.Get(pid)
+		if p_pin == nil {
+			continue
+		}
+
+		//binary search O(log q)
+		i := sort.Search(len(p_pin.nets), func(i int) bool { return p_pin.nets[i] >= nid2 })
+		if i < 0 || i >= len(p_pin.nets) {
+			continue
+		}
+
+		//retarget the pin to point at target
+		p_pin.nets[i] = nid1
+	}
+
+	//remove the source *without* removing pins, since they have been retargeted
+	nc.netlist.Remove(nid2, LNet[S, T]{})
+
+	return true
 }
--- go/lcircuit/lcircuit_test.go
+++ go/lcircuit/lcircuit_test.go
@@ -68,50 +68,291 @@ func TestAddMultipleGates(t *testing.T) {
 
 func TestAddRemoveGates(t *testing.T) {
 	circuit := &LCircuit[int, int]{}
-	gview := LCGateController[int, int]{circuit}
-	gview.gatetypes = testGates
+	gc := LCGateController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	gc.gatetypes = testGates
 
-	gview.AddGate("AND")
-	gview.AddGate("NOT")
-	gview.AddGate("LATCH")
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
 
-	gview.RemoveGate(1)
+	gc.RemoveGate(1)
 
-	gates := gview.ListGateIds()
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
 
 	if len(gates) != 2 || gates[0] != 0 || gates[1] != 2 {
-		t.Errorf("Expected 2 gates with ids 0 (AND) and 2 (LATCH), but found %v", gates)
+		t.Errorf("Expected 2 valid gates with ids 0 (AND) and 2 (LATCH), but found %v", gates)
 	}
-	if len(gview.netlist) != 3 {
-		t.Errorf("Expected 3 slots in netlist, got %d", len(gview.netlist))
+	if len(pins) != 7 {
+		t.Errorf("Expected 5 valid pins, got %v", pins)
 	}
-	if len(gview.pinlist) != 9 {
-		t.Errorf("Expected 9 slots in pinlist, got %d", len(gview.pinlist))
+	if len(gc.netlist) != 3 {
+		t.Errorf("Expected 3 slots in netlist, got %d", len(gc.netlist))
+	}
+	if len(gc.pinlist) != 9 {
+		t.Errorf("Expected 9 slots in pinlist, got %d", len(gc.pinlist))
 	}
 }
 
 func TestFillInRemovedGate(t *testing.T) {
 	circuit := &LCircuit[int, int]{}
-	gview := LCGateController[int, int]{circuit}
-	gview.gatetypes = testGates
+	gc := LCGateController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	gc.gatetypes = testGates
 
-	gview.AddGate("AND")
-	gview.AddGate("NOT")
-	gview.AddGate("LATCH")
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
 
-	gview.RemoveGate(1)
+	gc.RemoveGate(1)
 
-	gview.AddGate("LATCH")
+	gc.AddGate("LATCH")
 
-	gates := gview.ListGateIds()
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
 
 	if len(gates) != 3 || gates[0] != 0 || gates[1] != 1 || gates[2] != 2 {
-		t.Errorf("Expected 3 gates with ids 0 (AND), 1 (second LATCH), and 2 (first LATCH), but found %v", gates)
+		t.Errorf("Expected 3 valid gates with ids 0 (AND), 1 (second LATCH), and 2 (first LATCH), but found %v", gates)
 	}
-	if len(gview.netlist) != 3 {
-		t.Errorf("Expected 3 slots in netlist, got %d", len(gview.netlist))
+	if len(pins) != 11 {
+		t.Errorf("Expected 6 valid pins, got %v", pins)
 	}
-	if len(gview.pinlist) != 11 {
-		t.Errorf("Expected 11 slots in pinlist, got %d", len(gview.pinlist))
+	if len(gc.netlist) != 3 {
+		t.Errorf("Expected 3 slots in netlist, got %d", len(gc.netlist))
+	}
+	if len(gc.pinlist) != 11 {
+		t.Errorf("Expected 11 slots in pinlist, got %d", len(gc.pinlist))
+	}
+}
+
+func TestAddPinEmptyNet(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	pc := LCPinController[int, int]{circuit}
+
+	pc.AddPin(LABEL_EMPTY)
+
+	if len(pc.pinlist) != 1 {
+		t.Errorf("Expected 1 slot in pinlist, got %d", len(pc.pinlist))
+	}
+	if len(pc.pinlist[0].nets) != 0 {
+		t.Errorf("Expected pin 0 to have 0 nets, got %v", pc.pinlist[0].nets)
+	}
+}
+
+func TestFillInPin(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	pc := LCPinController[int, int]{circuit}
+
+	pc.AddPin(LABEL_EMPTY)
+	middle := pc.AddPin(LABEL_EMPTY)
+	pc.AddPin(LABEL_EMPTY)
+
+	pc.RemovePin(middle)
+
+	pc.AddPin(LABEL_EMPTY)
+	pins := pc.ListPins()
+
+	if len(pins) != 3 || pins[0] != 0 || pins[1] != 1 || pins[2] != 2 {
+		t.Errorf("Expected 2 gates with ids 0 (AND) and 2 (LATCH), but found %v", pins)
+	}
+	if len(pc.pinlist) != 3 {
+		t.Errorf("Expected 3 slots in netlist, got %d", len(pc.netlist))
+	}
+}
+
+func TestTamperGate(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	circuit.gatetypes = testGates
+
+	gc := LCGateController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+
+	//Add a gate and attempt to tamper with its pinout
+	gc.AddGate("AND")
+	r1 := pc.AddPin(0)
+	r2 := pc.RemovePin(0)
+
+	if r1 == LABEL_EMPTY || !r2 {
+		t.Errorf("Expected tampering to succeed. Add: %v, Remove: %v", r1, r2)
+	}
+	if len(pc.pinlist) != 4 {
+		t.Errorf("Expected 4 slots in pinlist, got %v", pc.pinlist)
+	}
+	if len(gc.netlist) != 1 {
+		t.Errorf("Expected 1 slot in netlist, got %v", gc.netlist)
+	}
+}
+
+func TestMergeFromEmpty(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	gc := LCGateController[int, int]{circuit}
+	nc := LCNetController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	gc.gatetypes = testGates
+
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+
+	result := nc.MergeTwo(0, LABEL_EMPTY)
+
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
+	if result {
+		t.Errorf("Expected merge to fail, but it succeeded")
+	}
+	if len(gates) != 3 || gates[0] != 0 || gates[1] != 1 || gates[2] != 2 {
+		t.Errorf("Expected 3 valid gates with ids 0 (AND), 1 (NOT), and 2 (LATCH), but found %v", gates)
+	}
+	if len(pins) != 9 {
+		t.Errorf("Expected 5 valid pins, got %v", pins)
+	}
+	if len(gc.netlist) != 3 {
+		t.Errorf("Expected 2 slots in netlist, got %v", gc.netlist)
+	}
+	if len(gc.pinlist) != 9 {
+		t.Errorf("Expected 9 slots in pinlist, got %v", gc.pinlist)
+	}
+	if len(gc.netlist[0].pins) != 3 || gc.netlist[0].pins[0] != 0 || gc.netlist[0].pins[1] != 1 || gc.netlist[0].pins[2] != 2 {
+		t.Errorf("Expected pins 0, 1, 2 on gate 0, got %v", gc.netlist[0].pins)
+	}
+}
+
+func TestMergeIntoEmpty(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	gc := LCGateController[int, int]{circuit}
+	nc := LCNetController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	gc.gatetypes = testGates
+
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+
+	result := nc.MergeTwo(LABEL_EMPTY, 0)
+
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
+	if result {
+		t.Errorf("Expected merge to fail, but it succeeded")
+	}
+	if len(gates) != 3 || gates[0] != 0 || gates[1] != 1 || gates[2] != 2 {
+		t.Errorf("Expected 3 valid gates with ids 0 (AND), 1 (NOT), and 2 (LATCH), but found %v", gates)
+	}
+	if len(pins) != 9 {
+		t.Errorf("Expected 5 valid pins, got %v", pins)
+	}
+	if len(gc.netlist) != 3 {
+		t.Errorf("Expected 2 slots in netlist, got %v", gc.netlist)
+	}
+	if len(gc.pinlist) != 9 {
+		t.Errorf("Expected 9 slots in pinlist, got %v", gc.pinlist)
+	}
+	if len(gc.netlist[0].pins) != 3 || gc.netlist[0].pins[0] != 0 || gc.netlist[0].pins[1] != 1 || gc.netlist[0].pins[2] != 2 {
+		t.Errorf("Expected pins 0, 1, 2 on gate 0, got %v", gc.netlist[0].pins)
+	}
+}
+
+func TestMergeTwo(t *testing.T) {
+	circuit := &LCircuit[int, int]{}
+	gc := LCGateController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	gc.gatetypes = testGates
+
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+
+	nc := LCNetController[int, int]{circuit}
+	result := nc.MergeTwo(0, 2)
+
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
+	if !result {
+		t.Errorf("Expected merge to succeed, but it failed")
+	}
+	if len(gates) != 2 || gates[0] != 0 || gates[1] != 1 {
+		t.Errorf("Expected 2 valid gates with ids 0 (AND) and 1 (NOT), but found %v", gates)
+	}
+	if len(pins) != 9 {
+		t.Errorf("Expected 9 valid pins, got %v", pins)
+	}
+	if len(gc.netlist) != 3 {
+		t.Errorf("Expected 2 slots in netlist, got %v", gc.netlist)
+	}
+	if len(gc.pinlist) != 9 {
+		t.Errorf("Expected 9 slots in pinlist, got %v", gc.pinlist)
+	}
+}
+
+func TestMergeMany(t *testing.T) {
+	//Test merging many nets, invalid and valid, into one
+	circuit := &LCircuit[int, int]{}
+	gc := LCGateController[int, int]{circuit}
+	pc := LCPinController[int, int]{circuit}
+	nc := LCNetController[int, int]{circuit}
+	gc.gatetypes = testGates
+
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+	gc.AddGate("AND")
+	gc.AddGate("NOT")
+	gc.AddGate("LATCH")
+
+	gc.RemoveGate(6)
+	gc.RemoveGate(10)
+
+	gates := gc.ListGateIds()
+	pins := pc.ListPins()
+
+	if len(gates) != 10 {
+		t.Errorf("Expected 10 valid gates, but found %v", gates)
+	}
+	if len(pins) != 31 {
+		t.Errorf("Expected 31 valid pins, got %v", pins)
+	}
+	if len(gc.netlist) != 12 {
+		t.Errorf("Expected 12 slots in netlist, got %v", gc.netlist)
+	}
+	if len(gc.pinlist) != 36 {
+		t.Errorf("Expected 36 slots in pinlist, got %v", gc.pinlist)
+	}
+
+	/*
+		Test:
+			-1: invalid id at the beginning of the list
+			6, 10: ids of removed gates
+			others: different gate types
+			not included: leaving certain gates alone
+
+		Desired effect:
+			0, 4, 5, 7, 8, 11 merged into 0. 10 gates becomes 5. Ids:
+				0, 1, 2, 3, 9
+			36 pins added, 5 removed. Pins are sustained through merge. Total 31 valid
+			12 nets added.
+	*/
+	nc.Merge([]Label{-1, 0, 4, 5, 6, 7, 8, 10, 11})
+
+	gates = gc.ListGateIds()
+	pins = pc.ListPins()
+	if len(gates) != 5 || gates[0] != 0 || gates[1] != 1 || gates[2] != 2 || gates[3] != 3 || gates[4] != 9 {
+		t.Errorf("Expected 5 valid gates with ids 0 (AND), 1 (NOT), 2 (LATCH), 3 (AND), and 9 (AND), but found %v", gates)
+	}
+	if len(pins) != 31 {
+		t.Errorf("Expected 31 valid pins, got %v", pins)
+	}
+	if len(gc.netlist) != 12 {
+		t.Errorf("Expected 12 slots in netlist, got %v", gc.netlist)
+	}
+	if len(gc.pinlist) != 36 {
+		t.Errorf("Expected 36 slots in pinlist, got %v", gc.pinlist)
 	}
 }
